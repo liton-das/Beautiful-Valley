66711cac1ffe87577fe041052157e38ed9f2faae
liton-das <litonchandra094@gmail.com>
2026-04-15 19:16:52 +0300

    fix bugs in reset-password and delete-single-user api done

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -201,6 +201,17 @@ const resetPasswordController = async(req,res)=>{
         return responseHeader.error(res)
     }
 }
+// delete single user controller
+ const deleteSingleUserController = async(req,res)=>{
+    try {
+        const {id} = req.params
+        const existUser = await User.findOneAndDelete({_id:id})
+        if(!existUser) return responseHeader.error(res,'User not found!',404)
+        return responseHeader.success(res,'User deleted successfully')
+    } catch (e) {
+        return responseHeader.error(res)
+    }
+ }
 module.exports = {
     registerUserController,
     verifyOtpController,
@@ -211,5 +222,6 @@ module.exports = {
     logoutController,
     searchUserController,
     forgotPasswordController,
-    resetPasswordController
+    resetPasswordController,
+    deleteSingleUserController
 }
