399e761b5df9267e56c16bab04fbe6a76cd70b6d
liton-das <litonchandra094@gmail.com>
2026-04-21 22:02:49 +0300

    update room data api not completed

--- server/src/controllers/roomController.js
+++ server/src/controllers/roomController.js
@@ -51,9 +51,67 @@ const createRoomPostController=async(req,res)=>{
      await createRoom.save()
      return responseHeader.success(res,'Room created successfully',createRoom)
 }
+// edit room by admin
+const editRoomByAdminController = async(req,res)=>{
+   try {
+      const {title,roomNumber,roomStatus,description,price,discount,bookingDate,bookingCancelDate} = req.body
+      const existUser = await Room.findById(req.user._id)
+      if(!existUser) return responseHeader.error(res,'User not found!',404)
+      const slug = slugGenerator(title)
+      if(title){
+         existUser.title = title
+         existUser.slug = slug
+      }
+      if(roomNumber){
+         existUser.roomNumber = roomNumber
+      }
+      if(roomStatus){
+         existUser.roomStatus = roomStatus
+      }
+      if(description){
+         existUser.description = description
+      }
+      const finalPrice = price - (price * (discount / 100))
+      if(price){
+         existUser.price = price
+         existUser.finalPrice = finalPrice
+      }
+      if(discount){
+         existUser.discount = discount
+      }
+      if(bookingDate && bookingCancelDate){
+         const start = new Date(bookingDate)
+         const end = new Date(bookingCancelDate)
+         if(start > end){
+            return responseHeader.error(res,'Booking cancel must be after booking!',400)
+         }
+         existUser.bookingDate=start
+         existUser.bookingCancelDate = end
+      }
+      // image edit 
+      const imageFile = req.files?.roomImage[0]
+      const videoFile = req.files?.roomVideo[0]
+      let imageData = '';
+      let videoData = '';
 
+      if (imageFile) {
+         imageData = await cloudinaryUploader('rooms/images',"image",imageFile)
+      }
+      if(videoFile){
+         videoData = await cloudinaryUploader('rooms/videos',"video",videoFile)
+      }
+      existUser.roomImg = imageData
+      existUser.roomVideo = videoData
+      await existUser.save()
+      return responseHeader.success(res,'Edit successfully')
+   } catch (e) {
+      console.log(e)
+      return responseHeader.error(res)
+   }
+}
 
 
 module.exports ={
-    createRoomPostController
+    createRoomPostController,
+    editRoomByAdminController
 }
--- server/src/routes/api/RoomApi.js
+++ server/src/routes/api/RoomApi.js
@@ -1,11 +1,13 @@
-const { createRoomPostController } = require('../../controllers/roomController')
+const { createRoomPostController, editRoomByAdminController } = require('../../controllers/roomController')
 const multer = require('multer')
 const { authMiddleware } = require('../../middlewares/authMiddleware')
+const roleCheckMiddleware = require('../../middlewares/roleCheckMiddleware')
 const upload = multer()
 const roomApi = require('express').Router()
 // create room api
 roomApi.post('/create-room',authMiddleware,upload.fields([{name:'roomImage',maxCount:1},{name:'roomVideo',maxCount:1}]),createRoomPostController)
-
+// edit room api
+roomApi.put('/edit-room',authMiddleware,roleCheckMiddleware('admin'),upload.fields([{name:'roomImage',maxCount:1},{name:'roomVideo',maxCount:1}]),editRoomByAdminController)
 
 
 
